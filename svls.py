389787f59cfb618d202505da503a00e5d551c3b7
ZheckSavalas <57382953+ZheckSavalas@users.noreply.github.com>
2019-11-05 14:04:56 +0700

    Add files via upload

---
+++ svls.py
@@ -0,0 +1,67 @@
+import os, sys
+
+print ("\033[1;32mSilahkan Masukkan Username & Password Anda")
+
+print ("\033[1;32matau silahkan Hubungi WA 6289691465691 ")
+
+username = 'ZheckS4valas1807'      
+
+password = 'S4lvalas'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mAlhmdllh sudah masuk juga..", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[1;32mMaaf Masukkan password Anda salah... [?]\033[00m"
+
+			print "Silahkan segera log-in kembali...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mMaaf Masukkan Username Anda salah... [?]\033[00m"
+
+		print "Silahkan segera log-in kembali...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
---
+++ zs.sh
@@ -0,0 +1,268 @@
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 svls.py
+echo
+# [SUBSCRIBE] Zacky Saksakame
+figlet -f standard FB Tools | lolcat
+echo
+echo  " ____________________-=[Private Tools]=-____________________" | lolcat
+echo  "|                                                           |" | lolcat
+echo  "|              Root®Autor      : Zheck|Savalas              |" | lolcat
+echo  "|              Youtube         : Zacky Saksakame            |" | lolcat
+echo  "|              Thanks to       : StussyXTeam                |" | lolcat
+echo  "|___________________________________________________________|" | lolcat
+date | lolcat
+echo
+echo $i"===================================================="
+echo $i"|"$me" @"$i" |"$me" WARNING!!! SEBELUM MENGGUNAKAN TOOLS INI" $i"|"$me" @"$i" |"
+echo $i"|"$me" @"$i" |"$me" \-SEBAIKNYA INSTALL DULU BAHAN-BAHANYA-/" $i"|"$me" @"$i" |"
+echo $i"===================================================="
+echo $i"|"$me" 0"$i" |"$pu"----------》!!!INSTALL BAHAN!!!《---------"$i"|"$me" 0"$i" |"
+echo $i"===================================================="
+echo $i"|"$me" 1"$i" |"$pu" BRUTEFORCE FB PHP"
+echo $i"|"$me" 2"$i" |"$pu" BRUTEFORCE FB v.1"
+echo $i"|"$me" 3"$i" |"$pu" BRUTEFORCE FB v.1.2"
+echo $i"|"$me" 4"$i" |"$pu" BRUTEFORCE FB v.1.3"
+echo $i"|"$me" 5"$i" |"$pu" BRUTEFORCE FB v.1.4"
+echo $i"|"$me" 6"$i" |"$pu" BRUTEFORCE FB "$me"[Root Access]"
+echo $i"────────────────────────────────────────────────────"
+echo $i"|"$me" 7"$i" |"$pu" YAHOO CLONING FACEBOOK"
+echo $i"|"$me" 8"$i" |"$pu" PROFILE GUARD FACEBOOK"
+echo $i"|"$me" 9"$i" |"$pu" AUTO REPORT FACBOOK"
+echo $i"|"$me"10"$i" |"$pu" AUTO REACTION FACEBOOK"
+echo $i"|"$me"11"$i" |"$pu" AUTO LIKE FACEBOOK"
+echo $i"|"$me"12"$i" |"$pu" AUTO KOMEN FACEBOOK"
+echo $i"|"$me"13"$i" |"$pu" INFORMATION FACEBOOK"
+echo $i"────────────────────────────────────────────────────"
+echo $i"|"$me"14"$i" |"$pu" PHISING FACEBOOK 1"
+echo $i"|"$me"15"$i" |"$pu" PHISING FACEBOOK 2"
+echo $i"|"$me"16"$i" |"$pu" PHISING FACEBOOK 3"
+echo $i"|"$me"17"$i" |"$pu" PHISING FACEBOOK 4"
+echo $i"===================================================="
+echo $i"|"$me"18"$i" |"$pu" EXIT"
+echo $i"===================================================="
+echo
+echo $me"┌==="$bi"["$i"ZHECK|SAVALAS"$bi"]"$me"======"$bi"["$i""Masukan Nomor""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 0 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " SUCCESS "|lolcat
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "SHUTDOWN"|lolcat
+
+echo $cy"Thanks For Using This Tools"
+sleep 2
+echo $i"If u find the bug please contact to me"
+sleep 2
+echo $ku"Youtube :"$i" Zacky Saksakame"
+echo $ku"Facebook :"$i" Zheck Savalas"
+echo $ku"Instagram :"$i" @saksakame1807"
+sleep 2
+echo $pur"STAY THE CRIME MY BOSS, SEE U !"
+exit
+fi
